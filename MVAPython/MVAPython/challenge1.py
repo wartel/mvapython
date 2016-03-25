a64babf457cb7ae910b71b64fd00156a69146f70
wartel <walterdewit@live.nl>
2016-03-25 09:38:57 +0100

    chalenge1

---
+++ MVAPython/MVAPython/challenge1.py
@@ -0,0 +1,6 @@
+#This code is the solution to the challenge 1
+print('There once was a movie star icon '\
+    '\nwho preferred to sleep with the light on.'\
+    '\nThey learned how to code'\
+    '\na device that sure glowed'\
+    '\nand lit up the night using Python!')
