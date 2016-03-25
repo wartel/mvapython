8411184b14a4faa248ed6cf72cda1c85ef24acc4
wartel <walterdewit@live.nl>
2016-03-25 15:14:26 +0100

    change4

---
+++ MVAPython/MVAPython/decisionplus.py
@@ -0,0 +1,10 @@
+team=input("Enter your favorite hockey team").upper()
+
+if team == "SENATORS":
+    print("Go Sens Go!")
+elif team == "RANGERS":
+    print("Go Rangers Go!")
+elif team == "BULLS":
+    print("Go Bulls Go!")
+else:
+    print("Hi there")
